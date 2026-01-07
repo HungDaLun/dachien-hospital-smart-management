c47287e7153c3f5159524314a5bf3c53e1e0ad73
Darren Hung <siriue0@gmail.com>
2026-01-07 21:21:54 +0800

    清理一次性測試檔案

--- add_summary_column.sql
+++
@@ -1,7 +0,0 @@
--- Add ai_summary to files table if it doesn't exist
-DO $$
-BEGIN
-    IF NOT EXISTS (SELECT 1 FROM information_schema.columns WHERE table_name = 'files' AND column_name = 'ai_summary') THEN
-        ALTER TABLE files ADD COLUMN ai_summary TEXT;
-    END IF;
-END $$;
